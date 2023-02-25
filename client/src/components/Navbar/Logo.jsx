07b0a06f74c5a8bbd5ec8bad8bd3f3df8e561829
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-25 21:05:49 +0100

    added link on logo to home

--- client/src/components/Navbar/Logo.jsx
+++ client/src/components/Navbar/Logo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import classes from "./Logo.module.css";
 import logo from "../../assets/logo-large.png";
+import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
     <div className={classes["logo-container"]}>
-      <img src={logo}></img>
+      <Link to="/">
+        <img src={logo}></img>
+      </Link>
     </div>
   );
 }
