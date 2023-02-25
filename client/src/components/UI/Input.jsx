7907a48633710769310fd09d8a072dfc024ea071
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-25 23:30:35 +0100

    fixed ref for Input

--- client/src/components/UI/Input.jsx
+++ client/src/components/UI/Input.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 import classes from "./Input.module.css";
 
-export default function Input({
-  id,
-  label,
-  type,
-  value,
-  onChange,
-  onBlur,
-  min,
-  max,
-}) {
-  return (
-    <div className={classes["input-container"]}>
-      <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        type={type}
-        min={min}
-        max={max}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-      />
-    </div>
-  );
-}
+const Input = React.forwardRef(
+  ({ id, label, type, value, onChange, onBlur, min, max }, ref) => {
+    return (
+      <div className={classes["input-container"]}>
+        <label htmlFor={id}>{label}</label>
+        <input
+          ref={ref}
+          id={id}
+          type={type}
+          min={min}
+          max={max}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      </div>
+    );
+  }
+);
+
+export default Input;
