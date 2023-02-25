692d5d81605e8399d9df2c0e77594a7524c0bd3d
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-26 00:40:21 +0100

    the sign up page works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import { ProvideAuth } from "./hooks/useAuth";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes className="app-routes">
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </div>
+    <ProvideAuth>
+      <div className="App">
+        <Navbar />
+        <Routes className="app-routes">
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </ProvideAuth>
   );
 }
 
--- client/src/components/User/SignUp/SignUpForm.jsx
+++ client/src/components/User/SignUp/SignUpForm.jsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../hooks/useAuth";
 import Button from "../../UI/Button";
 import Card from "../../UI/Card";
 import Input from "../../UI/Input";
 import classes from "./SignUpForm.module.css";
 
 export default function SignUpForm() {
+  const { signUp } = useAuth();
+  const navigate = useNavigate();
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const dobInputRef = useRef();
+
   function submitHandler(e) {
     e.preventDefault();
+    signUp(
+      {
+        name: nameInputRef.current.value,
+        email: emailInputRef.current.value,
+        password: passwordInputRef.current.value,
+        dateOfBirth: dobInputRef.current.value,
+      },
+      (user) => {
+        console.log(user);
+        navigate("/");
+      },
+      (err) => {
+        //TODO fix error messages
+        console.log(err);
+      }
+    );
   }
   return (
     <Card className={classes["sign-up-container"]}>
       <form>
         <h1>Sign up</h1>
-        <Input id="name" label="Name" type="text" />
-        <Input id="email" label="Email" type="email" />
-        <Input id="password" label="Password" type="password" />
+        <Input id="name" label="Name" type="text" ref={nameInputRef} />
+        <Input id="email" label="Email" type="email" ref={emailInputRef} />
+        <Input
+          id="password"
+          label="Password"
+          type="password"
+          ref={passwordInputRef}
+        />
         <Input
           id="dob"
           label="Date of birth"
           type="date"
+          ref={dobInputRef}
           min="1900-01-01"
           max={new Date().toISOString().split("T")[0]}
         />
