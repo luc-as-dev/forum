6ff46e870e8afd2b47c6024bf6f03a92cb19072c
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-26 18:13:49 +0100

    added logout, getMe and function for token

--- client/src/hooks/useAuth.jsx
+++ client/src/hooks/useAuth.jsx
@@ -1,11 +1,52 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const SERVER_URL = import.meta.env.VITE_SOME_SERVER_URL;
+
+const TOKEN_KEY = "token";
+
+function setToken(token) {
+  // TODO find a better solution for token
+  localStorage.setItem(TOKEN_KEY, token);
+}
+
+function getToken() {
+  // TODO find a better solution for token
+  return localStorage.getItem(TOKEN_KEY);
+}
+
+function clearToken() {
+  // TODO find a better solution for token
+  localStorage.removeItem(TOKEN_KEY);
+}
+
+function fixedUser(user) {
+  return {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    dateOfBirth: user.dateOfBirth,
+    profile: {
+      firstName: user.profile.firstName,
+      lastName: user.profile.lastName,
+      avatar: `${SERVER_URL}/${user.profile.avatar}`,
+      bio: user.profile.bio,
+    },
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  };
+}
 
 function useProvideAuth() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState([]);
   const [isLoading, setIsLoading] = useState(null);
+
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      getMe(token);
+    }
+  }, []);
 
   async function signUp(data, userCallback, errCallback) {
     setIsLoading(true);
@@ -18,8 +59,9 @@ function useProvideAuth() {
       setIsLoading(false);
       if (response.ok) {
         const { user, token } = await response.json();
-        user.token = token; // TODO find a better way to store token.
-        setUser(user);
+
+        setToken(token); // TODO find a better way to store token.
+        setUser(fixedUser(user));
         if (userCallback) userCallback(user);
         console.log(user);
       } else {
@@ -27,8 +69,8 @@ function useProvideAuth() {
         setError(error);
         if (errCallback) errCallback(error);
       }
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
       setIsLoading(false);
     }
   }
@@ -43,21 +85,69 @@ function useProvideAuth() {
       });
       if (response.ok) {
         const { user, token } = await response.json();
-        user.token = token; // TODO find a better way to store token.
-        setUser(user);
+        setToken(token); // TODO find a better way to store token.
+        setUser(fixedUser(user));
         if (userCallback) userCallback(user);
       } else {
         const error = await response.json();
         setError(error);
         if (errCallback) errCallback(error);
       }
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
     }
     setIsLoading(false);
   }
 
-  async function logout() {}
+  async function logout() {
+    setIsLoading(true);
+    const token = getToken();
+    try {
+      const response = await fetch(`${SERVER_URL}/users/logout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        clearToken(); // TODO find a better way to store token.
+        setUser(null);
+      } else {
+        console.log(err);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+  }
+
+  async function getMe() {
+    setIsLoading(true);
+    const token = getToken();
+    if (token) {
+      try {
+        const response = await fetch(`${SERVER_URL}/users/me`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.ok) {
+          const user = await response.json();
+          setUser(fixedUser(user));
+        } else {
+          const error = await response.json();
+          setError(error);
+          console.log(err);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setIsLoading(false);
+  }
 
   return {
     user,
