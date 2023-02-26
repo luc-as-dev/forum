c305df93569d22cebd81bae56c193f98f7e1955a
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-26 18:30:59 +0100

    test to deploy on render

---
+++ render-server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { config } from "dotenv";
+import "../database/index.js";
+import { router as userRouter } from "./routes/users.js";
+import customCors from "./middleware/customCors.js";
+
+config();
+
+const app = express();
+
+const { ORIGIN_WHITELIST, PORT } = process.env;
+
+app.use(customCors(ORIGIN_WHITELIST));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Started server on port ${PORT}`);
+});
