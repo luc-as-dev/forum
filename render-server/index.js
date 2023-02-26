1dab7649e77a1e7998fb090619c21b80c666637b
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-26 18:48:40 +0100

    updated from index to db.js in database

--- render-server/index.js
+++ render-server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
-import "../database/index.js";
+import "./database/db.js";
 import { router as userRouter } from "./routes/users.js";
 import customCors from "./middleware/customCors.js";
 
