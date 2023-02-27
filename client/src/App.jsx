1a9fe1e9d6fa95bb2f9a5ab087a514c06f51cd86
Lucas Karlsson <lucas.karlsson@post.com>
2023-02-27 21:09:14 +0100

    added empty user page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { ProvideAuth } from "./hooks/useAuth";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import User from "./pages/User";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/user" element={<User />} />
         </Routes>
       </div>
     </ProvideAuth>
---
+++ client/src/pages/User.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import MenuPage from "./template/MenuPage";
+import classes from "./User.module.css";
+
+export default function User() {
+  return <MenuPage></MenuPage>;
+}
