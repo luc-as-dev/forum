3d29c67af1726d5e71e3383af644ccb19d6636ce
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-02 20:27:06 +0100

    added basic empty footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,11 @@ import UserId from "./pages/UserId";
 import UserMe from "./pages/UserMe";
 import Users from "./pages/Users";
 
+const NO_FOOTER = ["/login", "/signUp"];
+
 function App() {
+  const location = useLocation();
+
   return (
     <ProvideAuth>
       <div className="App">
@@ -26,6 +30,7 @@ function App() {
               <Route path=":id" element={<UserId />} />
             </Route>
           </Routes>
+          {!NO_FOOTER.includes(location.pathname) && <Footer />}
         </div>
       </div>
     </ProvideAuth>
